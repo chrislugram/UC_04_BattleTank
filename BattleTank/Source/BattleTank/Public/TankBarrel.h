829f758427bb14c6379a8a50e86b6c6ce76c9acc
Christian <chrislugram@gmail.com>
2016-06-28 20:47:03 +0200

    + Change barrel component

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -8,11 +8,21 @@
 /**
  * 
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 
 public:
 	void Elevate(float DegreesPerSecond);
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Barrel Movement")
+	float MaxDegreesPerSecond = 20;
+
+	UPROPERTY(EditAnywhere, Category = "Barrel Movement")
+	float MaxDegreesElevation = 40;
+
+	UPROPERTY(EditAnywhere, Category = "Barrel Movement")
+	float MinDegreesElevation = 0;
 };
